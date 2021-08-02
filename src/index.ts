7eadb6bc638cd2b00550a8638f861e6d1b1c8011
Divyanshi8715 <86044369+Divyanshi8715@users.noreply.github.com>
2021-08-02 17:33:21 +0530

    abc

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,24 @@
+// jfhgfghfhgggg
+// import "./css/main.scss";
+// import {Application, Ticker} from 'pixi.js';
+// import {Games} from "./ts/Games";
+// window.onload = ()=>{
+//     const app = new Application({
+//         width:800,
+//         height:500,
+//         backgroundColor:0x00A300,
+//         //resizeTo:window,
+//         sharedTicker: true,
+//         sharedLoader:true,
+//         // resolution: window.devicePixelRatio
+//     });
+
+//     document.body.appendChild(app.view);
+
+//     const game = new Games(app);
+//     const ticker = Ticker.shared;
+//     ticker.add(game.update.bind(game));
+
 import "./css/main.scss";
 import {Application, Ticker} from 'pixi.js';
 import {Games} from "./ts/Games";
